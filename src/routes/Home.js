6f88da594fc4804189bdd23a9c5a9f1c5abffff1
tpstar <hanlee12@gmail.com>
2018-08-10 21:32:12 -0500

    test setTimeout to redirect to 2nd page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>home</h1>
-    </div>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isStillThere: true
+    }
+  };
+
+  componentDidMount() {
+    this.timeoutId = setTimeout(() => this.setState({ isStillThere: false }), 10000);
+  };
+
+  componentWillUnmount() {
+    this.setState({ isStillThere: true })
+    clearTimeout(this.timeoutId)
+  }
+
+  render() {
+    console.log(this.state.isStillThere);
+    if (!this.state.isStillThere) {
+      return <Redirect to="/sec" />
+    }
+    return (
+      <div>
+        <h1>home</h1>
+      </div>
+    );
+  }
+
 };
 
 export default Home;
